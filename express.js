c2739fc473417437e1af9ac69bced0fa9d8b7266
Himanshuu03 <himanshujangra2403@gmail.com>
2024-05-25 08:20:23 +0530

    scrape-master

---
+++ express.js
@@ -0,0 +1,69 @@
+import Express from "express";
+import puppeteer from "puppeteer";
+
+var app=Express();
+const port =8005;
+app.use(Express.json());
+
+
+app.post('/', async (req, res)=>{
+    const data=await req.body;
+    console.log(data);
+    const p=await start(data.search);
+    res.json(p);
+}    )
+app.get('/', async(req, res)=>{
+  res.send("working scrape master")
+})
+app.listen(port, ()=>{
+    console.log("working on port " + port);
+})
+
+
+
+
+
+
+async function start(search) {
+  const browser = await puppeteer.launch({  args: [
+      "--disable-setuid-sandbox",
+      "--no-sandbox",
+      "--single-process",
+      "--no-zygote",
+    ],
+    // executablePath: puppeteer.executablePath() 
+});
+    console.log(puppeteer.executablePath());
+  const page = await browser.newPage();
+  await page.goto("https://www.amazon.in");
+
+  await page.type("#twotabsearchtextbox", `${search}`);
+  await page.click(".nav-search-submit");
+  await page.waitForNavigation();
+  await page.waitForSelector(".s-result-item");
+
+  const images = await page.$$eval(".s-image", (texts) => {
+    return texts.map((x) => x.src);
+  });
+
+  const titles = await page.$$eval("h2.s-line-clamp-2", (texts) => {
+    return texts.map((x) => x.textContent);
+  });
+
+  const prices = await page.$$eval("span.a-price > span.a-offscreen", (texts) => {
+    return texts.map((x) => x.textContent);
+  });
+  
+  let product = [];
+  let i = 0;
+  while (i < images.length && i < prices.length && i < titles.length) {
+    product = [...product,{ title: titles[i], image: images[i], price: prices[i] }];
+    //
+    i++;
+  }
+  console.log(product);
+
+  await browser.close();
+  return product;
+}
+
---
+++ practice.js
@@ -0,0 +1,32 @@
+import puppeteer from "puppeteer";
+
+console.log("hello");
+
+async function start() {
+  const browser = await puppeteer.launch(); //launches a browser
+  const page = await browser.newPage(); //creates a new page in brower
+
+  //screenshot
+  // await page.goto("https://en.wikipedia.org/wiki/JavaScript"); // page visits site
+  // await page.screenshot({ path: "fullpage.jpeg", fullPage: true }); //takes screenshot
+
+  // //web scrapping text only
+  // await page.goto("https://learnwebcode.github.io/practice-requests/");
+  // const names = await page.evaluate(() => {
+  //   return Array.from(document.querySelectorAll(".info strong")).map(
+  //     (x) => x.textContent
+  //   );
+  // });
+  // console.log(names);
+
+  //store photos
+  await page.goto("https://learnwebcode.github.io/practice-requests/");
+  const photos = await page.$$eval("div.card > img", (imgs) => {
+    return imgs.map((x) => x.src);
+  });
+  console.log(photos);
+
+  await browser.close(); //closes browser
+}
+
+start();
